61451b8eb42464bb14b050f25fa936a89532f125
Andre Reis <andrebragareis@gmail.com>
2011-01-23 23:38:00 -0500

    Initial framework

--- Csim2.cpp
+++ Csim2.cpp
@@ -4,13 +4,355 @@ using namespace std;
 /*
  * TODO:
  * - store re-healing times in simulation
- * -- vector array w/ type(bestworst) srcVID dstVID rehealing
+ * -- vector array w/ type(best,worst) srcVID dstVID rehealing
  * -- sum all times at the end and match against actual simEndTime-simStartTime(packet 1)
+ *		(add a trigger at the beginning of AddPacket, conditional on VID)
  *
  */
 
 int main(int argc, char* argv[])
 {
+	list<VanetVehicle> Vehicles;
+	list<simEvent> eventList;
+	list<reHealingTime> statList;	// TODO this needs to be global
 
+	// get lambda from CLI
+	if(argc>=4) g_lambda=atof(argv[3]);
+	// highway length from CLI
+	if(argc>=3) g_length=(float)atoi(argv[2]) + g_margin;	// 1500m buffer on each side
+	// random variable seed from CLI
+	if(argc>=2) g_seed = atoi(argv[1]);
+
+	cout << fixed << setprecision(4);
+
+	cout << "Simulation Start" << '\n';
+	cout << "Parameters:" <<
+			"\n\tSpeed \t" << g_speed <<
+			"\n\tRange \t" << g_rrange <<
+			"\n\tLambda \t" << g_lambda <<
+			"\n\tStep \t" << g_step <<
+			"\n\tEnd \t" << g_simEndTime <<
+			"\n\tSeed \t" << g_seed <<
+			'\n';
+
+	cout << fixed << setprecision(1);
+
+	// one RSU every 1km
+	int nRSU = (g_length)/g_rsuDensity;
+
+	// simulation time from time to fill road plus enough to get message across
+	g_simEndTime = (int)(g_length/g_speed)*1000*1.10;	// ten percent more
+	g_simEndTime += 80*(g_length); 	// conservative 80 seconds per kilometer [s/km]*[m] = ms
+	g_simEndTime *= (0.0039/g_lambda);	// factor density
+
+	// Random variable init
+	boost::mt19937 rng(g_seed);
+	boost::exponential_distribution<float> exp( g_lambda*g_speed/1000 );
+	boost::variate_generator< boost::mt19937, boost::exponential_distribution<float> > expgen(rng,exp) ;
+		// now call expgen() to get a number
+	// Uniform distribution
+	boost::uniform_smallint<int> unif(0, g_rsuDensity);
+	boost::variate_generator< boost::mt19937, boost::uniform_smallint<int> > unifgen(rng, unif);
+		// now call unifgen() to get an int
+
+	// Packet start time must be enough for the road to fill
+	int packetStart = ((g_length-g_margin)/5000)*200*1000;
+
+
+	// Initial vehicle, RSU, packet setup
+	{
+		simEvent eventEndV ('V', packetStart, g_margin/2, 'W', ++g_vID);	// type, time, position, direction, vID
+		g_PacketEndVID=g_vID;	// this vehicle is the packet's destination
+		simEvent eventStartV ('V', packetStart, g_length-g_margin/2, 'W', ++g_vID);	// type, time, position, direction, vID
+		g_PacketStartVID=g_vID;	// this vehicle starts the packet
+
+		eventList.push_back(eventEndV);
+		eventList.push_back(eventStartV);
+
+		// Schedule creation of first packet
+		simEvent packetEvent1 ('P', packetStart, g_PacketStartVID, 1); 	// type, time, vID, pID
+		eventList.push_back(packetEvent1);
+
+		// randomize first RSU position with a Uniform distribution
+		float startingRSUpos = unifgen();
+		cout << "\tFirst RSU pos \t" << startingRSUpos << '\n';
+
+		// place RSUs
+		for(int i=0; i<nRSU; i++)
+		{
+			simEvent eventRSU ('R', 0, startingRSUpos+(float)i*g_rsuDensity, 'R', ++g_vID);	// type, time, position, direction, vID
+			eventList.push_back(eventRSU);
+		}
+	}
+
+	// Exponential vehicle generation event loop
+	{
+		// fill list with WEST-bound vehicles
+		{
+			int accum = 0;
+			while(accum < g_simEndTime) // fill event list with enough events
+			{
+				int expvalue = expgen();	// get exponential value
+				accum+= (expvalue<5*(1000/(g_lambda*g_speed))) ? expvalue : 5*(1000/(g_lambda*g_speed));	// crop expgen() to a maximum bound
+				simEvent eventWestVehicle ('V', accum, 0.0, 'W', ++g_vID);	// type, time, position, direction, vID
+				eventList.push_back(eventWestVehicle);
+			}
+		}
+		// fill list with EAST-bound vehicles
+		{
+			int accum = 0;
+			while(accum < g_simEndTime) // fill event list with enough events
+			{
+				int expvalue = expgen();	// get exponential value
+				accum+=(expvalue<5*(1000/(g_lambda*g_speed))) ? expvalue : 5*(1000/(g_lambda*g_speed));
+				simEvent eventEastVehicle ('V', accum, g_length, 'E', ++g_vID);	// type, time, position, direction, vID
+				eventList.push_back(eventEastVehicle);
+			}
+		}
+	}
+
+	// Sort event list
+	eventList.sort(eventCompare);
+
+	// Print event list
+	cout << "Event List:" << '\n';
+	for( list<simEvent>::iterator it = eventList.begin(); it != eventList.end(); ++it )
+	{
+	    cout << '\t' << it->eventType << ' ' << it->time;
+	    if(it->eventType=='V') cout << " pos " << it->position << " dir " << it->direction << " vID " << it->vehicleID << '\n';
+	    if(it->eventType=='R') cout << " pos " << it->position << " vID " << it->vehicleID << '\n';
+	    if(it->eventType=='P') cout << " vID " << it->vehicleID << " pID " << it->packetID << '\n';
+	}
+
+
+	// MAIN LOOP
+	for (g_simTime=0; g_simTime<=g_simEndTime; g_simTime+=g_step)
+	{
+		// Advance all vehicles on each step
+		if(g_simTime!=0)
+			AdvanceVehicles(&Vehicles, g_step);
+
+		// Process events
+		{
+			while( eventList.front().time <= g_simTime)
+			{	// fetch event to process
+				simEvent tempEvent = eventList.front();
+
+				// log event
+				{
+					cout << "INFO Processing event " << tempEvent.eventType << ' ' << tempEvent.time;
+					if(tempEvent.eventType=='V') cout << " pos " << tempEvent.position << " dir " << tempEvent.direction << " vID " << tempEvent.vehicleID << '\n';
+					if(tempEvent.eventType=='R') cout << " pos " << tempEvent.position << " vID " << tempEvent.vehicleID << '\n';
+					if(tempEvent.eventType=='P') cout << " vID " << tempEvent.vehicleID << " pID " << tempEvent.packetID << '\n';
+				}
+
+				// process event
+				{
+					switch(tempEvent.eventType)
+					{
+						case 'V':
+						{
+							VanetVehicle tempVeh(tempEvent.vehicleID, tempEvent.direction, tempEvent.position, false);	// vID, dir, pos, isRSU
+							Vehicles.push_back(tempVeh);
+							cout << "LOG " << ((float)g_simTime)/1000.0 << " V " << tempEvent.vehicleID << ' ' << tempEvent.position << ' ' << tempEvent.direction << '\n';
+							break;
+						}
+						case 'R':
+						{
+							VanetVehicle tempRSU(tempEvent.vehicleID, 'R', tempEvent.position, true);	// vID, dir, pos, isRSU
+							Vehicles.push_back(tempRSU);
+							cout << "LOG " << ((float)g_simTime)/1000.0 << " R " << tempEvent.vehicleID << ' ' << tempEvent.position << '\n';
+							break;
+						}
+						case 'P':
+							AddPacket(&Vehicles, tempEvent.vehicleID, tempEvent.packetID);
+							break;
+						default:
+							break;
+					}
+				}
+				// erase event
+				eventList.pop_front();
+			}	// end process event
+		}
+
+		// propagate packets on all vehicles
+		ReBroadcastPackets(&Vehicles);
+
+	}	// end MAIN_LOOP
+
+	// should never get here
 	return 1;
+}
+
+void AdvanceVehicles(list<VanetVehicle> *Vehicles, unsigned int delta)
+{
+	// Advance all Vehicles, skip RSUs
+    for( list<VanetVehicle>::iterator iter = Vehicles->begin();
+    		iter != Vehicles->end();
+    			++iter )
+    {
+    	if(iter->direction=='W')
+    		iter->position += ( (float)delta/1000 * g_speed ); 		// time is in ms
+    	else if(iter->direction=='E')
+        	iter->position -= ( (float)delta/1000 * g_speed ); 		// time is in ms
+
+    	if(g_simTime%(10*1000)==0)	// log 'C' every x*1000 seconds
+    		cout << "LOG " << ((float)g_simTime)/1000.0 << " C " << iter->vehicleID << ' ' << iter->position << '\n';
+    }
+}
+
+void AddPacket (list<VanetVehicle> *Vehicles, int vehicleID, int packetID)
+{
+	// move iter to desired vehicle ID
+	list<VanetVehicle>::iterator iter = Vehicles->begin();
+	while( iter->vehicleID != vehicleID ) iter++;
+
+	// see if the vehicle already has the packet
+	bool hasPacket=false; 	// hasPacket flag
+	for(vector<int>::iterator packetIter = iter->packetList.begin();
+			packetIter != iter->packetList.end();
+				++packetIter )
+				{
+					if(*packetIter == packetID ) { hasPacket=true; break; }
+				}
+
+	// add a packet to a vehicle's vector of packets, if it is not present
+    if(!hasPacket)
+    {
+    	iter->packetList.push_back(packetID);
+
+    	// log 'P' event
+    	cout << "LOG " << ((float)g_simTime)/1000.0 << " P " << iter->vehicleID << ' ' << iter->position << ' ' << packetID << '\n';
+
+    	// see if it is a special case, and count
+    	DoStatistics(Vehicles, vehicleID, packetID);
+
+    	// if is new, then instruct vehicle to rebroadcast it immediately
+    	BroadcastPacket(Vehicles, vehicleID, packetID, true);
+    }
+}
+
+void BroadcastPacket(list<VanetVehicle> *Vehicles, int srcVehicleID, int packetID, bool needsSort)
+{
+	// sort list of vehicles
+	if(needsSort) Vehicles->sort(vehiclePosCompare);
+
+	// move iter to desired vehicle ID
+	list<VanetVehicle>::iterator iter = Vehicles->begin();
+	while( iter->vehicleID != srcVehicleID ) iter++;
+
+	// tag position
+	float srcPosition = iter->position;
+
+	// we'll need these to be mutable
+	list<VanetVehicle>::iterator iterFW = iter;
+	list<VanetVehicle>::iterator iterBW = iter;
+
+	// broadcast forward
+	if( iterFW != Vehicles->end()) iterFW++;
+	while( iterFW->position <= srcPosition+250 && iterFW != Vehicles->end() )
+	{
+		AddPacket(Vehicles, iterFW->vehicleID, packetID);
+		iterFW++;
+	}
+
+	// broadcast backward
+	if( iterBW != Vehicles->begin()) iterBW--;
+	while( iterBW->position >= srcPosition-250 && iterBW != Vehicles->begin() )
+	{
+		AddPacket(Vehicles, iterBW->vehicleID, packetID);
+		iterBW--;
+	}
+
+}
+
+void ReBroadcastPackets(list<VanetVehicle> *Vehicles)
+{
+	// for each packet in each vehicle, rebroadcast that packet
+
+	// sort Vehicles list only once here
+	Vehicles->sort(vehiclePosCompare);
+
+	// Vehicle iterator
+    for( list<VanetVehicle>::iterator vehIter = Vehicles->begin();
+    		vehIter != Vehicles->end();
+    			++vehIter )
+    {
+    	// Packet iterator
+        for(vector<int>::iterator packetIter = vehIter->packetList.begin();
+        		packetIter != vehIter->packetList.end();
+        			++packetIter )
+        {
+        	// per vehicle, per packet {vehIter, packetIter} code goes here
+        	BroadcastPacket(Vehicles, vehIter->vehicleID, *packetIter, false);
+        }
+    }
+}
+
+void DoStatistics(list<VanetVehicle> *Vehicles, int srcVehicleID, int packetID)
+{
+	// get our vehicle
+	list<VanetVehicle>::iterator iter = Vehicles->begin();
+	while( iter->vehicleID != vehicleID ) iter++;
+
+	// 1st step: see if the vehicle is 'W' and is disconnected from the next vehicle
+	{
+		// get the position of the first vehicle (those in front of it do not count)
+    	list<VanetVehicle>::iterator startVeh = Vehicles->begin();
+    	while( startVeh->vehicleID != g_PacketStartVID ) startVeh++;
+
+    	// V is in the lane of interest lane, V is behind the starting vehicle
+    	if(iter->direction == 'W' && iter->position <= startVeh->position)
+    	{
+    		// verify if it is last vehicle in cluster
+    		bool isLastVehicle = true;
+    		{	// locate backward
+				list<VanetVehicle>::iterator iterBW = iter;
+				if( iterBW != Vehicles->begin()) iterBW--;	// sanity check
+				while( iterBW->position >= srcPosition-250 && iterBW != Vehicles->begin() )
+				{
+					if(iterBW->direction == 'W')
+						{isLastVehicle=false; break;}	// found a vehicle
+					iterBW--;	// did not find a vehicle, move
+				}
+    		}
+
+    		// if this vehicle is disconnected, the delay measures start here
+    		if(isLastVehicle)
+    		{
+				// TODO
+
+
+
+
+    		}	// END isLastVehicle
+    	}	// END dir=W & pos<startVehPos
+	}
+
+
+	// Finally: if this is the last vehicle, print logs and die
+	if(srcVehicleID == g_PacketEndVID)
+	{
+		// TODO
+		// PrintStatistics()
+		exit(1);
+	}
+
+}
+
+bool eventCompare (simEvent event1, simEvent event2)
+{
+	if(event1.time < event2.time)
+		return true;
+	else
+		return false;
+}
+
+bool vehiclePosCompare (VanetVehicle veh1, VanetVehicle veh2)
+{
+	if(veh1.position < veh2.position)
+		return true;
+	else
+		return false;
 }
--- Csim2.h
+++ Csim2.h
@@ -9,20 +9,22 @@
 
 using namespace std;
 
-// sim parameters
+// simulation parameters
 int g_simTime=0;			// in ms
-int g_simEndTime=2000*1000;	// in ms
+int g_simEndTime=UINT_MAX;	// in ms
 int g_step=100;				// 100ms step
-unsigned int g_BestCaseCount=0;
-unsigned int g_WorstCaseCount=0;
-int g_PacketStartVID=0;
 
 // highway parameters
 float g_speed=30.0;		// 30 m/s
 float g_rrange=250.0;	// 250m radio range
 float g_lambda=0.0039;	// vehicle density
-float g_length=6000;	// highway length
+float g_length=10000;	// highway length
+float g_margin=3000.0;	// side margin on highway length
 unsigned int g_vID=0;	// global vehicle ID counter
+unsigned int g_PacketStartVID=0;	// ID of vehicle that generates the packet
+unsigned int g_PacketEndVID=0;		// ID of destination vehicle
+unsigned int g_seed=1;				// RV seed
+unsigned int g_rsuDensity=1000;		// meters per RSU
 
 class VanetVehicle {
 public:
@@ -31,26 +33,38 @@ public:
 	char direction;
 	vector<int> packetList; 	// array of packets
 	VanetVehicle(int vid, char dir, float pos) {vehicleID=vid; direction=dir; position=pos;}
-	VanetVehicle(){vehicleID=0;position=0;direction='F';}
+	VanetVehicle(){vehicleID=0; position=0; direction='F';}
 };
 
 class simEvent {
 public:
-	char eventType; 	// 'V' new vehicle 'P' new packet
+	char eventType; 	// 'V' new vehicle; 'P' new packet
 	int time;			// in ms
-	float position;		// V
-	char direction;		// V
-	int vehicleID;		// V, P
-	int packetID;		// P
+	float position;		// V only
+	char direction;		// V only
+	int vehicleID;		// V & P
+	int packetID;		// P only
 	simEvent(char type, int t, float pos, char dir, int vid) {eventType=type; time=t; position=pos; direction=dir; vehicleID=vid; packetID=0;}
 	simEvent(char type, int t, int vid, int pid) {eventType=type; time=t; vehicleID=vid; packetID=pid; direction='P'; position=0.0;}
 };
 
-bool eventCompare (simEvent event1, simEvent event2);
-bool vehiclePosCompare (VanetVehicle veh1, VanetVehicle veh2);
+class reHealingTime {
+public:
+	char type;		// 'B' for best, 'W' for worst
+	int startTime;
+	int startVID;
+	int endVID;
+	int delay;
+	reHealingTime(char ty, int st, int sVID, int eVID) {type=ty; startTime=st; startVID=sVID; endVID=eVID; delay=INT_MIN;}
+};
+
 void AdvanceVehicles(list<VanetVehicle> *Vehicles, unsigned int delta);
 void AddPacket (list<VanetVehicle> *Vehicles, int vehicleID, int packetID);
 void BroadcastPacket(list<VanetVehicle> *Vehicles, int srcVehicleID, int packetID, bool needsSort);
 void ReBroadcastPackets(list<VanetVehicle> *Vehicles);
+void DoStatistics(list<VanetVehicle> *Vehicles, int srcVehicleID, int packetID);
+// class sorting functions
+bool eventCompare (simEvent event1, simEvent event2);
+bool vehiclePosCompare (VanetVehicle veh1, VanetVehicle veh2);
 
 #endif /* VANETCSIM_H_ */
